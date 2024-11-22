5cd551eaf96527d73289016c71101e67a2174ffa
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-11-22 14:43:45 +0100

    ADD basic page not found error and server error handler using middlewares

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const postsRouter = require('./routers/posts.js');
+const notFound = require('./middlewares/notFound.js');
+const errorsHandler = require('./middlewares/errorHandler.js');
 const app = express();
 const port = 3000;
 
@@ -7,6 +9,8 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/posts', postsRouter);
+app.use(notFound);
+app.use(errorsHandler);
 
 app.listen(port, function () {
     console.log('Server started...');
