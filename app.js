28b175150688f546080ed4c389a2ec9a1810ba7d
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-12-04 14:50:49 +0100

    ADD cors middleware

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const postsRouter = require('./routers/posts.js');
 const notFound = require('./middlewares/notFound.js');
 const errorsHandler = require('./middlewares/errorHandler.js');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
